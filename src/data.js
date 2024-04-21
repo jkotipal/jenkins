a39aba05fd5bd9e49b9cb2c6971cca507f7b086c
Hari0o <harisrikanth0@gmail.com>
2024-04-21 17:33:27 +0530

    jenkins tes2

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Barclets for Hands",
+    title: "Barclets for Legs",
     desc: "Barclets sale at 50% offer",
     bg: "green",
   },
