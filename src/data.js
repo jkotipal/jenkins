99b34435fea9a9343178ad9722e8d1d600ac622d
Hari <118122494+Hari0o@users.noreply.github.com>
2024-04-27 18:14:55 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "dinnama for Lambidjlk",
+    title: "Braceletes for Hands",
     desc: "Barclets sale at 50% offer",
     bg: "green",
   },
