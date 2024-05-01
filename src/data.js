575bd14233611afb9058e995828c7dcca1bb9884
Hari <118122494+Hari0o@users.noreply.github.com>
2024-05-01 13:27:55 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "testing for demo",
+    title: "test2 for demo",
     desc: "Barclets sale at 50% offer",
     bg: "green",
   },
