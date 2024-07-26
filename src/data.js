9094f68ef0bcf9c7877c0b015ed4151f5066bf8f
Hari <118122494+Hari0o@users.noreply.github.com>
2024-07-26 21:52:28 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const sliderItems = [
     id: "1",
     img: "assets/braclet/main.jfif",
     title: "sale on friends",
-    desc: "Gold price reduced",
+    desc: "Gold price reduced hurrY",
     bg: "green",
   },
   {
