fb211a82d0ee1271768cb567f93f1293495bee68
Hari <118122494+Hari0o@users.noreply.github.com>
2024-07-27 13:55:30 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const sliderItems = [
     id: "1",
     img: "assets/braclet/main.jfif",
     title: "sale on friends",
-    desc: "Docker tag test 1",
+    desc: "Docker tag test 2",
     bg: "green",
   },
   {
