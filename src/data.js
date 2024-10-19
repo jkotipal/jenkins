36f20327cdd98f04f928f6ad7e9233881c8b3532
Hari <118122494+Hari0o@users.noreply.github.com>
2024-10-19 13:01:17 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "sale on friends",
+    title: "ReBuild from scracth is successfull",
     desc: "This is now in new commit",
     bg: "green",
   },
