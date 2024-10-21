c67f8ec33df0431fce698129819b22b933a3c1ef
Hari <118122494+Hari0o@users.noreply.github.com>
2024-10-21 17:09:23 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Mail and slack alerts has been configured- test5",
+    title: "Mail and slack alerts has been configured succesfully",
     desc: "This is now in new commit",
     bg: "green",
   },
