8400dbb27bedd34a1c8a31a2d6854a7a3ff8ea42
Hari <118122494+Hari0o@users.noreply.github.com>
2024-11-27 16:25:26 +0000

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Helm charts have been successfully configured with ArgoCD",
+    title: "Helm charts have been successfully configured using ArgoCD",
     desc: "This is now in new commit",
     bg: "green",
   },
